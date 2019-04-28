53e5d36433156e68e0621d4f8b063d2bc43b8fa8
Tim Massey <tmassey101@hotmail.com>
2019-04-28 21:26:05 +0100

    added user login and logout

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, session, render_template, jsonify, url_for
+from flask import Flask, session, request, render_template, jsonify, url_for
+from flask_session import Session
 from flask_socketio import SocketIO, emit
 import requests, json
 
@@ -8,10 +9,39 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+curUser = []
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+
+    session.logged_in = True
+
+    curUser = request.form.get("username") 
+    session["curUser"] = curUser
+
+    return render_template("index.html", curUser=curUser)
+
+
+@app.route("/logout")
+def logout():
+
+    session.logged_in = False
+
+    curUser = []
+    session["curUser"] = []
+
+    return render_template("index.html", curUser=curUser)
 
 if __name__ == "__main__":  
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,25 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.querySelector('#submit').onclick = function() = {
+    username = document.querySelector('#name').value;
+    alert('Hello you');
 
-    // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
-    // When connected, configure buttons
-    socket.on('connect', () => {
-
-        // Each button should emit a "submit vote" event
-        document.querySelectorAll('button').forEach(button => {
-            button.onclick = () => {
-                const selection = button.dataset.vote;
-                socket.emit('submit vote', {'selection': selection});
-            };
-        });
-    });
-
-    // When a new vote is announced, add to the unordered list
-    socket.on('announce vote', data => {
-        const li = document.createElement('li');
-        li.innerHTML = `Vote recorded: ${data.selection}`;
-        document.querySelector('#votes').append(li);
-    });
-});
-
+};
